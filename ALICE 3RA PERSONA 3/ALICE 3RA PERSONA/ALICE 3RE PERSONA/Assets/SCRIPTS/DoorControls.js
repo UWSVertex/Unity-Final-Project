1ff0720749b93509b71395f8ae9b654152747351
UWSVertex <hernanfpm@gmail.com>
2016-11-05 12:25:56 -0600

    Changes
    Second Changes 1

---
+++ ALICE 3RA PERSONA 3/ALICE 3RA PERSONA/ALICE 3RE PERSONA/Assets/SCRIPTS/DoorControls.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+var doorOpen : boolean;
+
+
+
+function Start () {
+
+doorOpen = false;
+
+}
+
+function Update () {
+
+if (Input.GetButtonDown("E"))
+
+	{
+	animation.CrossFade("Open_Inside");
+	
+	}else{
+	
+	animation.CrossFade("Close_Outside");
+	}
+
+
+}
